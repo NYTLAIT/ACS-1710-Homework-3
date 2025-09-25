98bb3dd0a27e7d86cf056c5b47b54e6382868824
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-09-24 17:34:13 -0700

    Finished image filter challenege, not sure if done correctly however as dropdown disappears after working the first time (no longer able to filter an image after the first)

--- app.py
+++ app.py
@@ -151,22 +151,26 @@ def image_filter():
         # TODO: Get the user's chosen filter type (whichever one they chose in the form) and save
         # as a variable
         # HINT: remember that we're working with a POST route here so which requests function would you use?
-        filter_type = ''
+        filter_type = request.form.get('filter_type')
         
         # Get the image file submitted by the user
         image = request.files.get('users_image')
 
         # TODO: call `save_image()` on the image & the user's chosen filter type, save the returned
         # value as the new file path
+        file_path = save_image(image, filter_type)
 
         # TODO: Call `apply_filter()` on the file path & filter type
+        apply_filter(file_path, filter_type)
 
         image_url = f'./static/images/{image.filename}'
 
         context = {
             # TODO: Add context variables here for:
             # - The full list of filter types
+            'filter_types_dict': filter_types_dict,
             # - The image URL
+            'image_url': image_url
         }
 
         return render_template('image_filter.html', **context)
@@ -174,6 +178,7 @@ def image_filter():
     else: # if it's a GET request
         context = {
             # TODO: Add context variable here for the full list of filter types
+            'filter_types': filter_types
         }
         return render_template('image_filter.html', **context)
 
